869823219a3a959bcc2b5fb9aaa03f371d9785de
Dennis Gottschalk <dengot@gmx.de>
2019-06-30 17:47:04 +0200

    more optimization in TrainDataConverters

--- Bots/aibot/src/main/scala/bots/aibot/TrainDataConverter.scala
+++ Bots/aibot/src/main/scala/bots/aibot/TrainDataConverter.scala
@@ -27,7 +27,7 @@ class MarkovTrainDataConverter(rewardAdjustmentBase: Int, val discountRate: Doub
 
     val target = predict(reshape(stateTransition.state))
     val index = stateTransition.action.toDirection45
-    target.put(0, index, targetValue)
+    target.put(0, index, targetValue.max(0))
 
     new DataSet(reshape(stateTransition.state), target)
   }
@@ -41,11 +41,13 @@ class PredictionRewardAdjustmentDataConverter(rewardAdjustmentBase: Int) extends
     if (stateTransition.reward != 0) {
       val index = stateTransition.action.toDirection45
 
+      /*
       val targetValue = target.getDouble(0L, index.longValue())
       val reward: Double = stateTransition.reward.doubleValue() / rewardAdjustmentBase.doubleValue()
       target.put(0, index, targetValue + reward)
+      */
 
-      //target.put(0, index, if (stateTransition.reward > 0) 1d else 0d)
+      target.put(0, index, if (stateTransition.reward > 0) 1d else 0d)
     }
 
     new DataSet(reshape(stateTransition.state), target)
@@ -55,12 +57,22 @@ class PredictionRewardAdjustmentDataConverter(rewardAdjustmentBase: Int) extends
 
 class DirectRewardLastMoveDataConverter(rewardAdjustmentBase: Int) extends TrainDataConverter(rewardAdjustmentBase) {
 
-  private def zero(): Double = { 0d }
+  private def zero(): Double = {
+    0d
+  }
 
   override def toTrainData(stateTransition: StateTransition, predict: INDArray => INDArray): DataSet = {
 
     def createTarget(direction: Int, valueMatching: Double, valueOthers: () => Double) = {
       Direction45.ALL.map(dir => if (dir == direction) valueMatching else valueOthers())
+    }
+
+    def createRandomTargetButNotDirection(direction: Int) = {
+      var rand = direction
+      while (rand == direction) {
+        rand = Random.nextInt(Direction45.ALL.size)
+      }
+      Direction45.ALL.map(dir => if (dir == rand) Random.nextDouble() else 0d)
     }
 
     val dirIndex = stateTransition.action.toDirection45
@@ -69,7 +81,7 @@ class DirectRewardLastMoveDataConverter(rewardAdjustmentBase: Int) extends Train
     val target = stateTransition.reward match {
       case 0 => createTarget(dirIndex, Random.nextDouble(), zero)
       case reward: Int if (reward > 0) => createTarget(dirIndex, 1d, zero) //reward.doubleValue() / rewardAdjustmentBase.doubleValue()
-      case reward: Int if (reward < 0) => createTarget(dirIndex, 0d, Random.nextDouble)
+      case reward: Int if (reward < 0) => createRandomTargetButNotDirection(dirIndex) //createTarget(dirIndex, 0d, Random.nextDouble)
     }
 
     val labels = Nd4j.create(target.toArray, Array(1, target.length))
